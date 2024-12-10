6b61dcc77832b320011bbd8942b2df2029fe4bd6
Phillip English <phill.j.english@gmail.com>
2024-12-11 00:09:27 +0100

    added an h3

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <>
       <h1>This will be deployed</h1>
       <h2 style={{backgroundColor: "deeppink", color: "deepskyblue"}}>This has been added after Netlify Deloy</h2>
+      <h3>This is now also deployed on Vercel</h3>
     </>
   )
 }
